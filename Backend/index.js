508339a271629492c9562754158c91c835948d61
Shavandeb Kaiti <91772809+Emulsion24@users.noreply.github.com>
2025-01-24 06:36:15 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 const PORT=5000;
 const __dirname=path.resolve();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://tint-scholar.vercel.app"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
